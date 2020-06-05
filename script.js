140d8e8a4e5bd03f7c23f1b5064a1206734a987e
Samhita Joshi <46680993+sj-on@users.noreply.github.com>
2020-06-05 21:28:09 +0530

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+function note1(){
+    var audio = document.getElementById("note1");
+    audio.play();
+  }
+ 
+function note2(){
+    var audio = document.getElementById("note2");
+    audio.play();
+  }
+  
+function note3(){
+    var audio = document.getElementById("note3");
+    audio.play();
+  }  
+
+function note4(){
+    var audio = document.getElementById("note4");
+    audio.play();
+  }  
+
+function note5(){
+    var audio = document.getElementById("note5");
+    audio.play();
+  }  
+
+function note6(){
+    var audio = document.getElementById("note6");
+    audio.play();
+  }  
+
+function note7(){
+    var audio = document.getElementById("note7");
+    audio.play();
+  }  
