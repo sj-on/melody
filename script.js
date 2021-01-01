d4e72ccf5fe5c5c7cda4d70e818a8c1947b6cb7a
Samhita Joshi <46680993+sj-on@users.noreply.github.com>
2021-01-01 18:56:41 +0530

    happpy rotter

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 function note1(){
     var audio = document.getElementById("note1");
+    audio.currentTime = 0;
     audio.play();
   }
  
 function note2(){
     var audio = document.getElementById("note2");
+    audio.currentTime = 0;
     audio.play();
   }
   
 function note3(){
     var audio = document.getElementById("note3");
+    audio.currentTime = 0;
     audio.play();
   }  
 
 function note4(){
     var audio = document.getElementById("note4");
+    audio.currentTime = 0;
     audio.play();
   }  
 
 function note5(){
     var audio = document.getElementById("note5");
+    audio.currentTime = 0;
     audio.play();
   }  
 
 function note6(){
     var audio = document.getElementById("note6");
+    audio.currentTime = 0;
     audio.play();
   }  
 
 function note7(){
     var audio = document.getElementById("note7");
+    audio.currentTime = 0;
     audio.play();
   }  
